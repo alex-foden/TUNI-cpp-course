51d9bd1f933c00ef1365f248faed0388e564de20
Alex Foden <alex.foden@tuni.fi>
2024-01-14 19:31:28 +0200

    tehtävä (1) 1.7.1 (H)

--- student/01/first_submission/main.cpp
+++ student/01/first_submission/main.cpp
@@ -12,6 +12,7 @@ int main()
     std::cin >> age;
 
     std::cout << "Pleased to meet you, " << name << "!" << std::endl;
+    std::cout << "In your next birthday you'll be " << ++age << " years old!" << std::endl;
 
     return EXIT_SUCCESS;
 }
