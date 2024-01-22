3e8790a164b108cd2a3e4678d2e481f80581ff07
Alex Foden <alex.foden@tuni.fi>
2024-01-22 12:49:51 +0200

    2.3.2 läheisimmät tekijät

---
+++ student/02/factors/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter a positive number: ";
+    int number = 0;
+    cin >> number;
+    int tekija1 = 0;
+    int tekija2 = 0;
+
+    if (number <= 0)
+    {
+        cout << "Only positive numbers accepted" << endl;
+        return EXIT_FAILURE;
+    }
+
+    for (int factor1 = 1; factor1 * factor1 <= number; factor1++)
+    {
+        for (int factor2 = 1; factor1 * factor2 <= number; factor2++)
+        {
+            if (factor1 * factor2 == number)
+            {
+                tekija1 = factor1;
+                tekija2 = factor2;
+            }
+
+        }
+    }
+
+    cout << number << " = " << tekija1 << " * " << tekija2 << endl;
+
+    return EXIT_SUCCESS;
+}
