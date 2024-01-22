e254d6bec5c1a93fe66e8c2596e30ff01b69143b
Alex Foden <alex.foden@tuni.fi>
2024-01-22 13:23:23 +0200

    fizzbuzz mutta huono

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -6,13 +6,19 @@ int main()
     std::cout << "How many numbers would you like to have? ";
     int number = 0;
     std::cin >> number;
+    std::string output = "";
 
     for (int i = 1; i <= number; i++)
     {
-        if (i % 3 == 0) {std::cout << "zip" << std::endl;}
+        if (i % 3 == 0 and i % 7 == 0) {output = "zip boing";}
 
-        else {std::cout << i << std::endl;}
+        else if (i % 3 == 0) {output = "zip";}
 
+        else if (i % 7 == 0) {output = "boing";}
+
+        else {output = std::to_string(i);}
+
+        std::cout << output << std::endl;
     }
 
     return EXIT_SUCCESS;
