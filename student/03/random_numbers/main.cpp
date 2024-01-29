19a43a1c3bf2fa40c9bf3aba488ac0d2d4627455
Alex Foden <alex.foden@tuni.fi>
2024-01-29 15:26:20 +0200

    teht 3.2.2 random numbers

--- student/03/random_numbers/main.cpp
+++ student/03/random_numbers/main.cpp
@@ -4,14 +4,28 @@
 
 using namespace std;
 
-void produce_random_numbers(unsigned int lower, unsigned int upper)
+
+void produce_random_numbers(unsigned int lower, unsigned int upper, unsigned int seed)
 {
-    // Implement your function here
+    default_random_engine gen(seed);
+    uniform_int_distribution<int> distr(lower, upper);
+
+    while (true)
+    {
+        cout << "Your drawn random number is " << distr(gen) << endl;
+        cout << "Press q to quit or any other key to continue: ";
+        string input = "";
+        cin >> input;
+        if (input == "q")
+        {
+            break;
+        }
+    }
 }
 
 int main()
 {
-    unsigned int lower_bound, upper_bound;
+    unsigned int lower_bound, upper_bound, seed;
     cout << "Enter a lower bound: ";
     cin >> lower_bound;
     cout << "Enter an upper bound: ";
@@ -24,7 +38,11 @@ int main()
         return EXIT_FAILURE;
     }
 
-    produce_random_numbers(lower_bound, upper_bound);
+    cout << "Enter a seed value: ";
+    cin >> seed;
+    cout << endl;
+
+    produce_random_numbers(lower_bound, upper_bound, seed);
 
     return EXIT_SUCCESS;
 }
--- student/03/simple_library/book.hh
+++ student/03/simple_library/book.hh
@@ -7,22 +7,22 @@
 
 class Book
 {
-public:
-    Book(std::string author, std::string title);
-    ~Book();
+    public:
+        Book(std::string author, std::string title);
+        ~Book();
 
-    void print();
-    void loan(const Date& today);
-    void renew();
-    void give_back();
+        void print();
+        void loan(const Date& today);
+        void renew();
+        void give_back();
 
 
-private:
-    std::string author_;
-    std::string title_;
-    Date loan_date_;
-    Date return_date_;
-    bool is_available_;
+    private:
+        std::string author_;
+        std::string title_;
+        Date loan_date_;
+        Date return_date_;
+        bool is_available_;
 };
 
 #endif // BOOK_HH
