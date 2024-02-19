358f7e4e5972ba6931dac4cae707b85df6163a6b
Alex Foden <alex.foden@tuni.fi>
2024-02-19 20:56:59 +0200

    teht 6.1.1 tehty (palindromi, rekursion käyttö)

---
+++ student/06/network/network.pro
@@ -0,0 +1,8 @@
+TEMPLATE = app
+CONFIG += console c++14
+CONFIG -= app_bundle
+CONFIG -= qt
+QMAKE_CXXFLAGS += -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC
+
+SOURCES += \
+    main.cpp
---
+++ student/06/palindrome/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#ifndef RECURSIVE_FUNC
+#define RECURSIVE_FUNC
+#endif
+
+bool palindrome_recursive(std::string s)
+{
+    RECURSIVE_FUNC
+
+    if(s.size() <= 1)
+    {
+      return true;
+    }
+
+    if(s.front() != s.back())
+    {
+      return false;
+    }
+
+    s = s.substr(1, s.size() - 2);
+
+    palindrome_recursive(s);
+}
+
+
+#ifndef UNIT_TESTING
+int main()
+{
+    std::cout << "Enter a word: ";
+    std::string word;
+    std::cin >> word;
+
+    if(palindrome_recursive(word)){
+        std::cout << word << " is a palindrome" << std::endl;
+    } else {
+        std::cout << word << " is not a palindrome" << std::endl;
+    }
+}
+#endif
