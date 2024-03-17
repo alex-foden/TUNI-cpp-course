8e3b46d57a248e728caad41a81ccf772de4aeed2
Alex Foden <alex.foden@tuni.fi>
2024-03-17 15:40:02 +0200

    teht 9.6.1 tehty

--- student/09/arrays/array_operations.cpp
+++ student/09/arrays/array_operations.cpp
@@ -1,1 +1,60 @@
 #include "array_operations.hh"
+
+int greatest_v1(int *itemptr, int size)
+{
+    int greatest = *itemptr;
+    itemptr++;
+    for(int i = 0; i < size; i ++)
+    {
+        if(*itemptr > greatest)
+        {
+            greatest = *itemptr;
+        }
+        itemptr++;
+    }
+
+    return greatest;
+}
+
+int greatest_v2(int *itemptr, int *endptr)
+{
+    int greatest = *itemptr;
+    itemptr++;
+
+    while(itemptr < endptr)
+    {
+        if(*itemptr > greatest)
+        {
+            greatest = *itemptr;
+        }
+        itemptr++;
+    }
+
+    return greatest;
+}
+
+void copy(int *itemptr, int *endptr, int *targetptr)
+{
+    while(itemptr < endptr)
+    {
+        *targetptr = *itemptr;
+
+        itemptr++;
+        targetptr++;
+    }
+}
+
+void reverse(int *leftptr, int *rightptr)
+{
+    rightptr--;
+    while(leftptr < rightptr)
+    {
+        int temp = *leftptr;
+
+        *leftptr = *rightptr;
+        *rightptr = temp;
+
+        leftptr++;
+        rightptr--;
+    }
+}
